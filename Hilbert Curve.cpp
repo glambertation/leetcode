085d5d6d23e947d7ab3fe2fb7c21ba6b9b78a230
glambertation <1027368907@qq.com>
2019-03-26 10:24:38 +0800

    update 希尔伯特曲线

--- Hilbert Curve.cpp
+++ Hilbert Curve.cpp
@@ -8,12 +8,15 @@ int hilbertCurve(int x, int y, int iter) {
 	int borderLen = (1 << (iter - 1));
 
 	if(x>=borderLen && y>=borderLen)
-		return areaCnt*2 + hilbertCurve(x-borderLEn, y-borderLen, iter -1);
+	    return areaCnt*2 + hilbertCurve(x-borderLEn, y-borderLen, iter -1);
 	else if (x<borderLen && y >=borderLen)
-		return areaCnt + hilbertCurve(x, y-borderLen, iter-1);
+	    return areaCnt + hilbertCurve(x, y-borderLen, iter-1);
 	else if (x < borderLen && y < borderLen)
-		return hilbertCurve(y, x, iter -1);
+	    return hilbertCurve(y, x, iter -1);
 	else
-		return areaCnt*3 + hilbertCurve(borderLen -1 -y, 2*borderLen-1-x, iter-1);
+	//右下角区域 = 前一阶按照y=-x对称，然后右移2*borderLen - 1，上移borderLen - 1
+	// 设原来坐标(a,b) => (-b, -a) => (2*borderLen - 1 - b, borderLen - 1 - a) = (x, y)
+    	// => a = borderLen - 1 - y, b = 2*borderLen - 1 - x
+	    return areaCnt*3 + hilbertCurve(borderLen -1 -y, 2*borderLen-1-x, iter-1);
 
 }
